209158f11603c564cc9ce848a2c06ddd5d9ec8d1
nadko-dev <58465748+nadko-dev@users.noreply.github.com>
2023-04-23 13:35:33 +0200

    Second Changes

--- public/script.js
+++ public/script.js
@@ -1,11 +1,2 @@
+// This website has been designed and developed by Nadir Zulfugarli (www.nadirdev.com)
 "use scrict";
-
-let language = document.querySelector(".lang");
-
-language.addEventListener("click", function () {
-  if (language.getAttribute("src") === "./assets/uk.png") {
-    language.setAttribute("src", "./assets/pl.png");
-  } else {
-    language.setAttribute("src", "./assets/uk.png");
-  }
-});
