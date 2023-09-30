d2e189b86941661fa6e67a15e5f78e44f92144b3
nadko-dev <58465748+nadko-dev@users.noreply.github.com>
2023-09-30 20:43:26 +0200

    Inital deploy

--- public/script.js
+++ public/script.js
@@ -1,2 +1,36 @@
-// This website has been designed and developed by Nadir Zulfugarli (www.nadirdev.com)
+// This website has been designed and developed by Nadir Zulfugarli https://www.nadirdev.com
+
 "use scrict";
+
+// Slide show
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let s;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (s = 0; s < slides.length; s++) {
+    slides[s].style.display = "none";
+  }
+  for (s = 0; s < dots.length; s++) {
+    dots[s].className = dots[s].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
